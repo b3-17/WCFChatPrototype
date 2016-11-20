459e62e3228f594259cf25dd84bc647b89a9862b
joe <you@example.com>
2016-11-20 15:36:13 -0800

    failing test example for channel creation

--- ChatServicesTests/ChannelTests.cs
+++ ChatServicesTests/ChannelTests.cs
@@ -1,14 +1,36 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Linq;
+using Services;
+using Models;
 
 namespace ChatServicesTests
 {
 	[TestFixture()]
 	public class ChannelTests
 	{
+		[TestFixtureSetUp]
+		public void SetUp()
+		{ 
+		
+		}
+
+		[TestFixtureTearDown]
+		public void CleanUp()
+		{ 
+		
+		}
+
 		[Test()]
 		public void CreateChannel()
 		{
+			IChatService service = new ChatService();
+			Channel channel = new Channel { ChannelName = "test channel" };
+			Assert.AreEqual(0, service.Channels.Count(), "the channel list should be empty on start up");
+			service.CreateChannel(channel);
+
+			Assert.IsTrue(1, service.Channels.Count(), "the chat channel list was not updated");
+			Assert.AreEqual("test channel", service.Channels.FirstOrDefault().ChannelName, "the channel name was incorrect");
 		}
 	}
 }
